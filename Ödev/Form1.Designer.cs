7571929385570bb1ec5c6a37616eab7f186580b5
Klecier <Klecier@hotmail.com>
2023-05-23 00:26:03 +0300

    Add project files.

---
+++ Ödev/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.matdgrutxt = new System.Windows.Forms.TextBox();
+            this.fizikdgrutxt = new System.Windows.Forms.TextBox();
+            this.kimyadgrutxt = new System.Windows.Forms.TextBox();
+            this.edebdgrutxt = new System.Windows.Forms.TextBox();
+            this.matynlstxt = new System.Windows.Forms.TextBox();
+            this.fizikynlstxt = new System.Windows.Forms.TextBox();
+            this.kimyaynlstxt = new System.Windows.Forms.TextBox();
+            this.edebynlstxt = new System.Windows.Forms.TextBox();
+            this.matsnctxt = new System.Windows.Forms.TextBox();
+            this.fiziksnctxt = new System.Windows.Forms.TextBox();
+            this.kimyasnctxt = new System.Windows.Forms.TextBox();
+            this.edebiyatsnctxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.hesapla = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(25, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matematik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(25, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Fizik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(25, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kimya";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(25, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Edebiyat";
+            // 
+            // matdgrutxt
+            // 
+            this.matdgrutxt.BackColor = System.Drawing.Color.Silver;
+            this.matdgrutxt.Location = new System.Drawing.Point(130, 42);
+            this.matdgrutxt.Name = "matdgrutxt";
+            this.matdgrutxt.Size = new System.Drawing.Size(100, 20);
+            this.matdgrutxt.TabIndex = 1;
+            // 
+            // fizikdgrutxt
+            // 
+            this.fizikdgrutxt.BackColor = System.Drawing.Color.Silver;
+            this.fizikdgrutxt.Location = new System.Drawing.Point(130, 75);
+            this.fizikdgrutxt.Name = "fizikdgrutxt";
+            this.fizikdgrutxt.Size = new System.Drawing.Size(100, 20);
+            this.fizikdgrutxt.TabIndex = 1;
+            // 
+            // kimyadgrutxt
+            // 
+            this.kimyadgrutxt.BackColor = System.Drawing.Color.Silver;
+            this.kimyadgrutxt.Location = new System.Drawing.Point(130, 110);
+            this.kimyadgrutxt.Name = "kimyadgrutxt";
+            this.kimyadgrutxt.Size = new System.Drawing.Size(100, 20);
+            this.kimyadgrutxt.TabIndex = 1;
+            // 
+            // edebdgrutxt
+            // 
+            this.edebdgrutxt.BackColor = System.Drawing.Color.Silver;
+            this.edebdgrutxt.Location = new System.Drawing.Point(130, 146);
+            this.edebdgrutxt.Name = "edebdgrutxt";
+            this.edebdgrutxt.Size = new System.Drawing.Size(100, 20);
+            this.edebdgrutxt.TabIndex = 1;
+            // 
+            // matynlstxt
+            // 
+            this.matynlstxt.BackColor = System.Drawing.Color.Silver;
+            this.matynlstxt.Location = new System.Drawing.Point(263, 42);
+            this.matynlstxt.Name = "matynlstxt";
+            this.matynlstxt.Size = new System.Drawing.Size(100, 20);
+            this.matynlstxt.TabIndex = 1;
+            // 
+            // fizikynlstxt
+            // 
+            this.fizikynlstxt.BackColor = System.Drawing.Color.Silver;
+            this.fizikynlstxt.Location = new System.Drawing.Point(263, 75);
+            this.fizikynlstxt.Name = "fizikynlstxt";
+            this.fizikynlstxt.Size = new System.Drawing.Size(100, 20);
+            this.fizikynlstxt.TabIndex = 1;
+            // 
+            // kimyaynlstxt
+            // 
+            this.kimyaynlstxt.BackColor = System.Drawing.Color.Silver;
+            this.kimyaynlstxt.Location = new System.Drawing.Point(263, 110);
+            this.kimyaynlstxt.Name = "kimyaynlstxt";
+            this.kimyaynlstxt.Size = new System.Drawing.Size(100, 20);
+            this.kimyaynlstxt.TabIndex = 1;
+            // 
+            // edebynlstxt
+            // 
+            this.edebynlstxt.BackColor = System.Drawing.Color.Silver;
+            this.edebynlstxt.Location = new System.Drawing.Point(263, 146);
+            this.edebynlstxt.Name = "edebynlstxt";
+            this.edebynlstxt.Size = new System.Drawing.Size(100, 20);
+            this.edebynlstxt.TabIndex = 1;
+            // 
+            // matsnctxt
+            // 
+            this.matsnctxt.BackColor = System.Drawing.Color.Silver;
+            this.matsnctxt.Location = new System.Drawing.Point(421, 41);
+            this.matsnctxt.Name = "matsnctxt";
+            this.matsnctxt.Size = new System.Drawing.Size(100, 20);
+            this.matsnctxt.TabIndex = 1;
+            // 
+            // fiziksnctxt
+            // 
+            this.fiziksnctxt.BackColor = System.Drawing.Color.Silver;
+            this.fiziksnctxt.Location = new System.Drawing.Point(421, 75);
+            this.fiziksnctxt.Name = "fiziksnctxt";
+            this.fiziksnctxt.Size = new System.Drawing.Size(100, 20);
+            this.fiziksnctxt.TabIndex = 1;
+            // 
+            // kimyasnctxt
+            // 
+            this.kimyasnctxt.BackColor = System.Drawing.Color.Silver;
+            this.kimyasnctxt.Location = new System.Drawing.Point(421, 109);
+            this.kimyasnctxt.Name = "kimyasnctxt";
+            this.kimyasnctxt.Size = new System.Drawing.Size(100, 20);
+            this.kimyasnctxt.TabIndex = 1;
+            // 
+            // edebiyatsnctxt
+            // 
+            this.edebiyatsnctxt.BackColor = System.Drawing.Color.Silver;
+            this.edebiyatsnctxt.Location = new System.Drawing.Point(421, 145);
+            this.edebiyatsnctxt.Name = "edebiyatsnctxt";
+            this.edebiyatsnctxt.Size = new System.Drawing.Size(100, 20);
+            this.edebiyatsnctxt.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(127, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Doğru";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(260, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 18);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Yanlış";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(418, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 18);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Net sonuç";
+            // 
+            // hesapla
+            // 
+            this.hesapla.BackColor = System.Drawing.Color.Silver;
+            this.hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.hesapla.Location = new System.Drawing.Point(76, 216);
+            this.hesapla.Name = "hesapla";
+            this.hesapla.Size = new System.Drawing.Size(546, 32);
+            this.hesapla.TabIndex = 2;
+            this.hesapla.Text = "Hesapla";
+            this.hesapla.UseVisualStyleBackColor = false;
+            this.hesapla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.edebiyatsnctxt);
+            this.groupBox1.Controls.Add(this.kimyasnctxt);
+            this.groupBox1.Controls.Add(this.edebynlstxt);
+            this.groupBox1.Controls.Add(this.kimyaynlstxt);
+            this.groupBox1.Controls.Add(this.fiziksnctxt);
+            this.groupBox1.Controls.Add(this.edebdgrutxt);
+            this.groupBox1.Controls.Add(this.fizikynlstxt);
+            this.groupBox1.Controls.Add(this.matsnctxt);
+            this.groupBox1.Controls.Add(this.kimyadgrutxt);
+            this.groupBox1.Controls.Add(this.matynlstxt);
+            this.groupBox1.Controls.Add(this.fizikdgrutxt);
+            this.groupBox1.Controls.Add(this.matdgrutxt);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(76, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(546, 195);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(677, 307);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.hesapla);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox matdgrutxt;
+        private System.Windows.Forms.TextBox fizikdgrutxt;
+        private System.Windows.Forms.TextBox kimyadgrutxt;
+        private System.Windows.Forms.TextBox edebdgrutxt;
+        private System.Windows.Forms.TextBox matynlstxt;
+        private System.Windows.Forms.TextBox fizikynlstxt;
+        private System.Windows.Forms.TextBox kimyaynlstxt;
+        private System.Windows.Forms.TextBox edebynlstxt;
+        private System.Windows.Forms.TextBox matsnctxt;
+        private System.Windows.Forms.TextBox fiziksnctxt;
+        private System.Windows.Forms.TextBox kimyasnctxt;
+        private System.Windows.Forms.TextBox edebiyatsnctxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button hesapla;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Ödev/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (matdgrutxt.Text == string.Empty)
+            {
+                MessageBox.Show("Matematik doğru sayısını giriniz");
+            }
+            else if (matynlstxt.Text == string.Empty)
+            {
+                MessageBox.Show("Matematik yanlış sayısını giriniz");
+            }
+
+            else if (fizikdgrutxt.Text == string.Empty)
+            {
+                MessageBox.Show("Fizik Doğru sayısını giriniz");
+            }
+            else if (fizikynlstxt.Text == string.Empty)
+            {
+                MessageBox.Show("Fizik yanlış sayısını giriniz");
+            }
+
+            else if (kimyadgrutxt.Text == string.Empty)
+            {
+                MessageBox.Show("Kimya doğru sayısını giriniz");
+            }
+            else if (kimyaynlstxt.Text == string.Empty)
+            {
+                MessageBox.Show("Kimya yanlış sayısını giriniz");
+
+            }
+            else if (edebdgrutxt.Text == string.Empty)
+            {
+                MessageBox.Show("Edebiyat doğru sayısını giriniz");
+            }
+            else if (edebynlstxt.Text == string.Empty)
+            {
+                MessageBox.Show("Edebiyat yanlış sayısını giriniz");
+            }
+
+            else
+            {
+
+                double matd = Convert.ToDouble(matdgrutxt.Text);
+                double maty = Convert.ToDouble(matynlstxt.Text);
+                double mnet = matd - (maty / 4);
+
+
+                double fizd = Convert.ToInt32(fizikdgrutxt.Text);
+                double fizy = Convert.ToInt32(fizikynlstxt.Text);
+                double fnet = fizd - (fizy / 4);
+
+
+                double kimd = Convert.ToInt32(kimyadgrutxt.Text);
+                double kimy = Convert.ToInt32(kimyaynlstxt.Text);
+                double knet = kimd - (kimy / 4);
+
+
+                double eded = Convert.ToInt32(edebdgrutxt.Text);
+                double edey = Convert.ToInt32(edebynlstxt.Text);
+                double enet = eded - (edey / 4);
+                edebiyatsnctxt.Text = enet.ToString();
+                kimyasnctxt.Text = kimd.ToString();
+                fiziksnctxt.Text = fnet.ToString();
+                matsnctxt.Text = mnet.ToString();
+
+
+                if (mnet < 10)
+                { 
+                    matsnctxt.BackColor = Color.LightCoral;
+                    matsnctxt.Enabled = false;
+                }
+                else if (mnet > 10)
+                {
+                    matsnctxt.BackColor = Color.Silver;                
+                    matsnctxt.Enabled = Enabled;
+                }
+
+                if (fnet < 10)
+                {
+                    fiziksnctxt.BackColor = Color.LightCoral;
+                    fiziksnctxt.Enabled = false;
+                }
+                else if (fnet > 10)
+                {
+                    fiziksnctxt.BackColor= Color.Silver;
+                    fiziksnctxt.Enabled= Enabled;
+                }
+
+                if (knet < 10)
+                {
+                    kimyasnctxt.BackColor = Color.LightCoral;
+                    kimyasnctxt.Enabled = false;
+                }
+                else if (knet > 10)
+                {
+                    kimyasnctxt.BackColor = Color.Silver;
+                    kimyasnctxt.Enabled = Enabled;
+                }
+
+                if (enet < 10)
+                {
+                    edebiyatsnctxt.BackColor = Color.LightCoral;
+                    edebiyatsnctxt.Enabled = false;
+                }
+                else if (enet > 10)
+                {
+                    edebiyatsnctxt.BackColor = Color.Silver;
+                    edebiyatsnctxt.Enabled = Enabled;
+                }
+                
+            }
+
+        }
+    }
+}
